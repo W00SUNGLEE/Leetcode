ec10df44e4c82c700b19e80bdb568bc6ddadd0aa
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-14 21:00:49 +0900

    Time: 1175 ms (8.42%), Space: 13.9 MB (11.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -9,17 +9,20 @@ class Solution:
             return 1
         
         else:
+            """
             for i in range(2, n+1):
                 fib_array.append(fib_array[i-2]+fib_array[i-1])
             return fib_array[n]
-            
-            
             """
+            
+            
             if len(fib_array) > n:       
                 return fib_array[n-2] + fib_array[n-1]
-            elif len(fib_array) == n
+            
+            elif len(fib_array) == n:
                 fib_array.append(fib_array[n-2] + fib_array[n-1])
-                retrun fib_array[n]
+                return fib_array[n]
+            
             else:
-                fib()
-            """
+                return self.fib(n-2) + self.fib(n-1)
+           
