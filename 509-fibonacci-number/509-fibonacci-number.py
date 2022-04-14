a944d64171626e16a4a2d589b845f26d93719d3f
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-14 21:01:42 +0900

    Time: 37 ms (75.18%), Space: 14 MB (11.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -9,13 +9,15 @@ class Solution:
             return 1
         
         else:
-            """
+            
+            #bottom -> up
             for i in range(2, n+1):
                 fib_array.append(fib_array[i-2]+fib_array[i-1])
             return fib_array[n]
+            
+            
+            #top -> down
             """
-            
-            
             if len(fib_array) > n:       
                 return fib_array[n-2] + fib_array[n-1]
             
@@ -25,4 +27,5 @@ class Solution:
             
             else:
                 return self.fib(n-2) + self.fib(n-1)
+            """
            
