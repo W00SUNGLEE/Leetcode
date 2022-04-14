6d2a34e3ce49c80efca4016b808e82218498ff7b
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-14 20:43:23 +0900

    Time: 831 ms (72.65%), Space: 28.5 MB (10.12%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -3,6 +3,20 @@ import sys
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         best_sum = -sys.maxsize -1
+        current_sum = 0
+        for x in nums:
+            current_sum = max(x, current_sum + x)
+            best_sum = max(best_sum, current_sum)
+            
+        return best_sum
+
+
+"""
+import sys
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        best_sum = -sys.maxsize -1
         #best_start = 0
         #best_end = 0
         current_sum = 0
@@ -21,3 +35,4 @@ class Solution:
                 
             print(best_sum)
         return best_sum
+"""
