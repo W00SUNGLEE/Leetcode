be4ed645d9b41204881364cdb6bd74fcb1a031e9
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-17 20:31:02 +0900

    Time: 136 ms (86.11%), Space: 14.8 MB (91.10%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -13,42 +13,4 @@ class Solution:
             else: #numbers[index1] + numbers[index2] > target
                 index2 -= 1
                 
-                
-        return -1
-            
-        
-        
-        """
-        distinct = [numbers[0], numbers[1]]
-        for i in range(2, len(numbers)):
-            if numbers[i] == distinct[i-1] and numbers[i] == distinct[i-2]:
-                continue
-            distinct.append(numbers[i])
-        
-        print(distinct)
-        """
-        """
-        distinct = list(set(numbers))
-        
-        print(distinct)
-        
-        for i in range(len(distinct)):
-            for j in range(i, len(distinct)):
-                if distinct[i] + distinct[j] == target:
-                    x_index = i
-                    y_index = j
-        """
-        
-        """
-        for i in range(len(numbers)):
-            for j in range(i+1, len(numbers)):  
-                if numbers[i] + numbers[j] == target:
-                    return [i+1, j+1]
-        """
-        n = len(numbers)
-        for i in range(n):
-            for j in range(i+1, n):
-                if numbers[i] + numbers[j] == target:
-                    return [i+1, j+1]
-        
         return -1
