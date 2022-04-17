5107aeb3ea1d3035d9daf2dfbf78cbb56d95e20d
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-17 20:53:36 +0900

    Time: 47 ms (70.18%), Space: 14.5 MB (97.99%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -4,9 +4,10 @@ class Solution:
         print(tmp_s)
         
         for i in range(len(tmp_s)):
-            tmp_s[i] = list(tmp_s[i])
-            tmp_s[i].reverse()
-            tmp_s[i] = "".join(tmp_s[i])
+            #tmp_s[i] = list(tmp_s[i])
+            #tmp_s[i].reverse() 
+            #tmp_s[i] = "".join(tmp_s[i])
+            tmp_s[i] = "".join(reversed(tmp_s[i]))
             
         return " ".join(tmp_s)
         
