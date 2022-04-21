45ae1547f58e4153a46dd65198dd71e1d83ef542
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-21 21:18:12 +0900

    Time: 37 ms (69.34%), Space: 14 MB (22.00%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -2,6 +2,33 @@ from collections import defaultdict
 
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        arr = []
+        
+        cnt = 0
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                cnt += 1
+                arr.append([s1[i], s2[i]]) 
+                
+            if cnt > 2:
+                return False
+            
+        if cnt == 0:
+            return True
+        
+        elif cnt == 2:        
+            if arr[0][0] == arr[1][1] and arr[0][1] == arr[1][0]:
+                return True
+            
+            else:
+                return False
+
+        else:
+            return False
+        
+        
+        
+        """
         dict1 = defaultdict(int)
         dict2 = defaultdict(int)
         
@@ -17,3 +44,5 @@ class Solution:
         
         else:
             return False
+            
+        """
