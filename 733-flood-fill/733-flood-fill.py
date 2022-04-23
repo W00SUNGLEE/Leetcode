3a2fa8bf69d9b98712b649b46de78416bbfba58a
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-23 19:51:30 +0900

    Time: 135 ms (18.37%), Space: 14.1 MB (40.36%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -3,32 +3,29 @@ class Solution:
         if image[sr][sc] == newColor:
             return image
         
-        self.dfs(image, sr, sc, newColor)
-        
-        return image
-    
-    
-    def dfs(self, image: List[List[int]], sr: int, sc: int, newColor: int):
-        oldColor = image[sr][sc]
-        image[sr][sc] = newColor
+        def dfs(self, image: List[List[int]], sr: int, sc: int, newColor: int):
+            oldColor = image[sr][sc]
+            image[sr][sc] = newColor
         
         
-        if sr > 0:
-            if image[sr-1][sc] == oldColor:
-                Solution.dfs(self, image, sr-1, sc, newColor)
+            if sr > 0:
+                if image[sr-1][sc] == oldColor:
+                    dfs(self, image, sr-1, sc, newColor)
         
-        if sr < len(image) - 1:
-            if image[sr+1][sc] == oldColor:
-                Solution.dfs(self, image, sr+1, sc, newColor)
+            if sr < len(image) - 1:
+                if image[sr+1][sc] == oldColor:
+                    dfs(self, image, sr+1, sc, newColor)
         
-        if sc > 0 :
-            if image[sr][sc-1] == oldColor:
-                Solution.dfs(self, image, sr, sc-1, newColor)
+            if sc > 0 :
+                if image[sr][sc-1] == oldColor:
+                    dfs(self, image, sr, sc-1, newColor)
         
-        if sc < len(image[0]) - 1:
-            if image[sr][sc+1] == oldColor:
-                Solution.dfs(self, image, sr, sc+1, newColor)
+            if sc < len(image[0]) - 1:
+                if image[sr][sc+1] == oldColor:
+                    dfs(self, image, sr, sc+1, newColor)
+        
+        dfs(self, image, sr, sc, newColor)
         
         
-            
         
+        return image    
