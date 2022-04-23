f14a9949d58fb9fb685d7a7a885725119139e685
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-23 19:53:20 +0900

    Time: 149 ms (9.62%), Space: 14.2 MB (40.36%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -3,29 +3,27 @@ class Solution:
         if image[sr][sc] == newColor:
             return image
         
-        def dfs(image: List[List[int]], sr: int, sc: int, newColor: int):
+        def dfs(sr: int, sc: int, newColor: int):
             oldColor = image[sr][sc]
             image[sr][sc] = newColor
         
         
             if sr > 0:
                 if image[sr-1][sc] == oldColor:
-                    dfs(image, sr-1, sc, newColor)
+                    dfs(sr-1, sc, newColor)
         
             if sr < len(image) - 1:
                 if image[sr+1][sc] == oldColor:
-                    dfs(image, sr+1, sc, newColor)
+                    dfs(sr+1, sc, newColor)
         
             if sc > 0 :
                 if image[sr][sc-1] == oldColor:
-                    dfs(image, sr, sc-1, newColor)
+                    dfs(sr, sc-1, newColor)
         
             if sc < len(image[0]) - 1:
                 if image[sr][sc+1] == oldColor:
-                    dfs(image, sr, sc+1, newColor)
+                    dfs(sr, sc+1, newColor)
         
-        dfs(image, sr, sc, newColor)
-        
-        
+        dfs(sr, sc, newColor)
         
         return image    
