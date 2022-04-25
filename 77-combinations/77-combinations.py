db11a037338f204d54d9179f13116e5f36866a5a
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-25 19:04:47 +0900

    Time: 531 ms (49.41%), Space: 16 MB (51.63%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -3,22 +3,20 @@ class Solution:
         
         solution = []
         
-        tmp = [0]
+        tmp = []
         
-        check = [0] * (n+1)
-        
-        def comb(v):
+        def comb(v, index):
             if v == k:
-                solution.append([tmp[i] for i in range(1,len(tmp))])
+                solution.append([i for i in tmp])
                 
             else:
-                for i in range(1, n+1):
-                    if i not in tmp and i > tmp[len(tmp)-1]:
+                for i in range(index+1, n+1):
+                    if i not in tmp:
                         tmp.append(i)
-                        comb(v+1)
+                        comb(v+1, i)
                         tmp.pop()
         
-        comb(0)
+        comb(0, 0)
         
         return solution
                         
