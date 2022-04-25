7946ca21bd70b9a326472457ac157ce67fb7f541
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-04-25 19:33:32 +0900

    Time: 76 ms (58.86%), Space: 15.2 MB (46.21%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        
+        solution = []
+        
+        tmp = []
+        
+        def backtracking(v):
+            if v == len(s):
+                solution.append("".join(list(i for i in tmp)))
+                                
+            else:
+                if s[v].isalpha():
+                    tmp.append(s[v].lower())
+                    backtracking(v+1)
+                    tmp.pop()
+                    
+                    tmp.append(s[v].upper())
+                    backtracking(v+1)
+                    tmp.pop()
+                                
+                                
+                else:
+                    tmp.append(s[v])
+                    backtracking(v+1)
+                    tmp.pop()
+                                
+        backtracking(0)
+                                
+        return solution
