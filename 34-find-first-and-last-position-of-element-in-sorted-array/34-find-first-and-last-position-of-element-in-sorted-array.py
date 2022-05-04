eda534203f92b42803d38fa3ccf7ac0c59aed3e9
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-05-04 12:54:15 +0900

    Time: 92 ms (79.07%), Space: 15.4 MB (52.22%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,6 +1,5 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        print(nums)
         answer = [-1, -1]
         
         for i in range(len(nums)):
@@ -10,6 +9,5 @@ class Solution:
                 else:
                     answer[1] = i
                     
-        print(answer)
         return answer
                 
