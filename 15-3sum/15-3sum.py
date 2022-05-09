eeb39b9648dd04701f7a76130e90491b7b530898
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-05-09 19:49:10 +0900

    Time: 731 ms (89.57%), Space: 18.1 MB (73.19%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -5,7 +5,7 @@ class Solution:
         nums.sort()
 
         answer = []
-                          
+        
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
