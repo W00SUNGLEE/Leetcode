6e3440f04505c88dcb38706a58b10cdd047477f6
W00SUNGLEE <101493000+W00SUNGLEE@users.noreply.github.com>
2022-05-12 14:36:25 +0900

    Time: 200 ms (48.20%), Space: 14.9 MB (51.01%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -6,16 +6,12 @@ class Solution:
         j = 0
         
         while i < len(firstList) and j < len(secondList):
-            print(firstList[i], secondList[j])
-            
             
             if firstList[i][0] > secondList[j][1]:
-                print(1)
                 j += 1
                 continue
                 
             if firstList[i][1] < secondList[j][0]:
-                print(2)
                 i += 1
                 continue
                 
@@ -24,22 +20,18 @@ class Solution:
             tmp.append(max(firstList[i][0], secondList[j][0]))
                 
             if firstList[i][1] < secondList[j][1]:
-                print(3)
                 tmp.append(firstList[i][1])
                 i += 1
                     
             elif firstList[i][1] > secondList[j][1]:
-                print(4)
                 tmp.append(secondList[j][1])
                 j += 1
                     
             else: #firstList[i][1] == secondList[j][1]:
-                print(5)
                 tmp.append(firstList[i][1])
                 i += 1
                 j += 1
                     
             answer.append(tmp)
-            print(i, j)
                 
         return answer
